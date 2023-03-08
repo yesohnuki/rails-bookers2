08fc88a03fb52b3a475eff7d4797130b04560fd2
EC2 Default User <ec2-user@ip-172-31-37-102.ap-northeast-1.compute.internal>
2023-03-08 09:31:09 +0000

    [Update]

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,16 +1,16 @@
 class FavoritesController < ApplicationController
 
   def create
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.new(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.new(book_id: @book.id)
     favorite.save
     @books = Book.all
     #redirect_back(fallback_location: root_path)
   end
 
   def destroy
-    book = Book.find(params[:book_id])
-    favorite = current_user.favorites.find_by(book_id: book.id)
+    @book = Book.find(params[:book_id])
+    favorite = current_user.favorites.find_by(book_id: @book.id)
     favorite.destroy
     @books = Book.all
     #redirect_back(fallback_location: root_path)
